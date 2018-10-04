49ea0acef3c341de1443dbb77e3ee136ac1650d1
mihail <misha.kukuev@gmail.com>
2018-10-04 16:36:57 +0300

    small fixies

--- app/controllers/RootController.java
+++ app/controllers/RootController.java
@@ -4,17 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import play.libs.Json;
 import play.mvc.*;
 
+
 public class RootController extends Controller {
 
-    public Result links() {
-        JsonNode response = Json.parse("{\n" +
-                "  \"_links\": {\n" +
-                "    \"self\": { \"href\": \"/sayings\" },\n" +
-                "    \"random\": { \"href\": \"/sayings/random\" },\n" +
-                "    \"add\": { \"href\": \"/sayings/new\" }\n" +
-                "  }\n" +
-                "}");
-        return ok(response);
-    }
+    private JsonNode defaultResponse = Json.parse("{\n" +
+            "  \"_links\": {\n" +
+            "    \"self\": { \"href\": \"/sayings\" },\n" +
+            "    \"random\": { \"href\": \"/sayings/random\" },\n" +
+            "    \"add\": { \"href\": \"/sayings/new\" }\n" +
+            "  }\n" +
+            "}");
 
+    public Result links() {
+        return ok(defaultResponse);
+    }
 }
